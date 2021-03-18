990e614eab0f59d0bb36e29c062054f585550ac0
xenone <rosenkranz1999@gmail.com>
2021-03-18 18:03:35 -0400

    added a bunch of moebius and converted to png

--- walls/patterngen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://www.imagemagick.org/script/formats.php#builtin-images
-convert -size 32x32 pattern:hs_diagonal \
--fill "$(clr -x color0)" -opaque black   -fill "$(clr -x color10)" -opaque white \
-newpattern.png
-#convert -size 1920x1080 tile:pattern.gif pattern_bg.jpg
